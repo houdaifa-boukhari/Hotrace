c51c35ff49299a3ec047808c4dd462ad2d822db5
Anouar Et-Taleb <aet-tale@c2r4p5.1337.ma>
2025-04-20 10:44:38 +0100

    addng my_get

---
+++ my_get/get_next_line_bonus.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aet-tale <aet-tale@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/16 19:40:10 by aet-tale          #+#    #+#             */
+/*   Updated: 2024/01/30 15:51:14 by aet-tale         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <limits.h>
+
+int		ft_strchr(char *s, char c);
+int		ft_strlen(char *s);
+char	*ft_strjoin(char *s1, char *s2);
+char	*get_next_line(int fd);
+char	*ft_get_line(char *str);
+char	*ft_new_str(char *str);
+char	*ft_get_buff(int fd, char *str);
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+
+#endif
