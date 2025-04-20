f823650a618f901be0741a7670b9fde66b35f8d4
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2025-04-20 14:58:46 +0100

    FIX nor for utilic

--- utilice.c
+++ utilice.c
@@ -1,102 +1,128 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utilice.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hel-bouk <hel-bouk@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/20 13:20:52 by hel-bouk          #+#    #+#             */
+/*   Updated: 2025/04/20 14:58:02 by hel-bouk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "hotrace.h"
-#include <stdlib.h>
-#include <unistd.h>
 
-#define READ_SIZE 8192
-#define INITIAL_ALLOC 128
+static int	ft_strlen(char *str)
+{
+	int	len;
 
-/* Helper for ft_putstr_fd */
-static int strlength(char *str) {
-    int len = 0;
-    if (str) while (str[len]) len++;
-    return len;
+	len = 0;
+	if (str)
+	{
+		while (str[len])
+			len++;
+		return (len);
+	}
+	return (0);
 }
 
-void ft_putstr_fd(char *str, int fd) {
-    int len = strlength(str);
-    if (len > 0) {
-        str[len - 1] = '\0';
-        write(fd, str, len - 1);
-    }
+void	ft_putstr_fd(char *str, int fd)
+{
+	int	len;
+
+	len = ft_strlen(str);
+	if (len > 0)
+	{
+		str[len - 1] = '\0';
+		write(fd, str, len - 1);
+	}
 }
 
-void *my_memchr(const void *s, int c, size_t n) {
-    const unsigned char *p = s;
-    while (n-- > 0) {
-        if (*p == (unsigned char)c)
-            return (void *)p;
-        p++;
-    }
-    return NULL;
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	const unsigned char	*p;
+
+	p = s;
+	while (n-- > 0)
+	{
+		if (*p == (unsigned char)c)
+			return ((void *)p);
+		p++;
+	}
+	return (NULL);
 }
 
-void *my_memcpy(void *dest, const void *src, size_t n) {
-    unsigned char *d = dest;
-    const unsigned char *s = src;
-    while (n-- > 0)
-        *d++ = *s++;
-    return dest;
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	unsigned char		*d;
+	const unsigned char	*s;
+
+	s = src;
+	d = dest;
+	while (n-- > 0)
+		*d++ = *s++;
+	return (dest);
 }
 
-void *my_realloc(void *ptr, size_t old_size, size_t new_size) {
-    if (new_size == 0) {
-        free(ptr);
-        return NULL;
-    }
-    void *new_ptr = malloc(new_size);
-    if (!new_ptr) return NULL;
-    if (ptr) {
-        size_t copy_size = old_size < new_size ? old_size : new_size;
-        my_memcpy(new_ptr, ptr, copy_size);
-        free(ptr);
-    }
-    return new_ptr;
+void	*ft_realloc(void *ptr, size_t old_size, size_t new_size)
+{
+	void	*new_ptr;
+	size_t	copy_size;
+
+	if (new_size == 0)
+		return (free(ptr), NULL);
+	new_ptr = (void *)malloc(new_size);
+	if (!new_ptr)
+		return (NULL);
+	if (ptr)
+	{
+		if (old_size < new_size)
+			copy_size = old_size;
+		else
+			copy_size = new_size;
+		ft_memcpy(new_ptr, ptr, copy_size);
+		free(ptr);
+	}
+	return (new_ptr);
 }
 
-/* Helpers for get_line */
-static int refill_buffer(char *buf, ssize_t *len, ssize_t *pos) {
-    *len = read(0, buf, READ_SIZE);
-    *pos = 0;
-    return *len > 0;
-}
+// char *get_line(void)
+// {
+//     int done;
+//     char *line;
+//     size_t alloc;
+//     ssize_t line_len;
+//     static char buf[READ_SIZE];
+//     static ssize_t len;
+// 	static ssize_t pos;
 
-static int resize_line(char **line, size_t *alloc, size_t needed) {
-    if (needed < *alloc) return 1;
-    char *new = my_realloc(*line, *alloc, *alloc * 2);
-    if (!new) return 0;
-    *line = new;
-    *alloc *= 2;
-    return 1;
-}
-
-char *get_line(void) {
-    static char buf[READ_SIZE];
-    static ssize_t len = 0, pos = 0;
-    size_t alloc = INITIAL_ALLOC;
-    char *line = malloc(alloc);
-    ssize_t line_len = 0;
-    int done = 0;
-
-    if (!line) return NULL;
-    while (!done) {
-        if (pos >= len && !refill_buffer(buf, &len, &pos)) break;
-        while (pos < len && !done) {
-            if (!resize_line(&line, &alloc, line_len + 1)) {
-                free(line);
-                return NULL;
-            }
-            line[line_len] = buf[pos++];
-            done = (line[line_len++] == '\n');
-        }
-    }
-    if (line_len == 0) {
-        free(line);
-        return NULL;
-    }
-    line[line_len] = '\0';
-    if (line_len + 1 < alloc / 2) {
-        char *tmp = my_realloc(line, alloc, line_len + 1);
-        if (tmp) line = tmp;
-    }
-    return line;
-}
+// 	len = 0;
+// 	pos = 0;
+// 	done = 0;
+// 	line_len = 0;
+// 	alloc = INITIAL_ALLOC;
+// 	line = (char *)malloc(alloc);
+//     if (!line)
+// 		return (NULL);
+//     while (!done)
+// 	{
+//         if (pos >= len && !refill_buffer(buf, &len, &pos))
+// 			break ;
+//         while (pos < len && !done)
+// 		{
+//             if (!resize_line(&line, &alloc, line_len + 1))
+//                 return (free(line),  NULL);
+//             line[line_len] = buf[pos++];
+//             done = (line[line_len++] == '\n');
+//         }
+//     }
+//     if (line_len == 0)
+//        return (free(line),  NULL);
+//     line[line_len] = '\0';
+//     if (line_len + 1 < alloc / 2) {
+//         char *tmp = ft_realloc(line, alloc, line_len + 1);
+//         if (tmp)
+// 			line = tmp;
+//     }
+//     return (line);
+// }
---
+++ utilice1.c
@@ -0,0 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utilice1.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hel-bouk <hel-bouk@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/20 14:57:16 by hel-bouk          #+#    #+#             */
+/*   Updated: 2025/04/20 14:58:19 by hel-bouk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "hotrace.h"
+
+static int	refill_buffer(char *buf, ssize_t *len, ssize_t *pos)
+{
+	*len = read(0, buf, READ_SIZE);
+	*pos = 0;
+	return (*len > 0);
+}
+
+static int	resize_line(char **line, size_t *alloc, size_t needed)
+{
+	char	*new;
+
+	if (needed < *alloc)
+		return (1);
+	new = ft_realloc(*line, *alloc, *alloc * 2);
+	if (!new)
+		return (0);
+	*line = new;
+	*alloc *= 2;
+	return (1);
+}
+
+static int	handle_buffer(t_line *line_data, t_buffer *buffer, int *done)
+{
+	while (buffer->pos < buffer->len && !(*done))
+	{
+		if (!resize_line(&line_data->line, &line_data->alloc,
+				line_data->line_len + 1))
+			return (0);
+		line_data->line[line_data->line_len] = buffer->buf[buffer->pos++];
+		*done = (line_data->line[line_data->line_len++] == '\n');
+	}
+	return (1);
+}
+
+static char	*finalize_line(t_line *line_data)
+{
+	char	*tmp;
+
+	if (line_data->line_len == 0)
+		return (free(line_data->line), NULL);
+	line_data->line[line_data->line_len] = '\0';
+	if (line_data->line_len + 1 < (long)(line_data->alloc / 2))
+	{
+		tmp = ft_realloc(line_data->line, line_data->alloc, line_data->line_len
+				+ 1);
+		if (tmp)
+			line_data->line = tmp;
+	}
+	return (line_data->line);
+}
+
+char	*get_line(void)
+{
+	static t_buffer	buffer = {{0}, 0, 0};
+	t_line			line_data;
+	int				done;
+
+	done = 0;
+	line_data.line_len = 0;
+	line_data.alloc = INITIAL_ALLOC;
+	line_data.line = (char *)malloc(line_data.alloc);
+	if (!line_data.line)
+		return (NULL);
+	while (!done)
+	{
+		if (buffer.pos >= buffer.len && !refill_buffer(buffer.buf, &buffer.len,
+				&buffer.pos))
+			break ;
+		if (!handle_buffer(&line_data, &buffer, &done))
+			return (free(line_data.line), NULL);
+	}
+	return (finalize_line(&line_data));
+}
